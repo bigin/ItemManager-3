3125735ca3539a7608354946ff5e240bb3811c29
Juri Ehret <juri.ehret@gmail.com>
2018-01-14 21:38:31 +0100

    no message

--- imanager/inc/_inc.php
+++ imanager/inc/_inc.php
@@ -1,39 +1,9 @@
 <?php
-
-if(!isset($_SESSION)){session_start();}
-if(!isset($_SESSION['cat']) || is_null($_SESSION['cat'])) $_SESSION['cat'] = null;
-
-/*register_plugin(
-	$thisfile,
-	'ItemManager',
-	IM_VERSION_GS,
-	'Juri Ehret',
-	'http://ehret-studio.com',
-	'A simple flat-file framework for GetSimple-CMS',
-	'imanager',
-	'im_render_backend'
-);*/
-
-// activate actions
-//add_action('admin-pre-header', 'ajaxGetLists');
-//add_action('nav-tab', 'createNavTab', array($thisfile, $thisfile, 'Manager'));
-//register_style('jqui', IM_SITE_URL.'plugins/'.$thisfile.'/upload/js/jquery-ui/jquery-ui.css',
-//	GSVERSION, 'screen');
-//register_style('imstyle', IM_SITE_URL.'plugins/'.$thisfile.'/css/im-styles.css',
-//	GSVERSION, 'screen');
-//register_style('blueimp',  IM_SITE_URL.'plugins/'.$thisfile.'/css/blueimp-gallery.min.css',
-//	GSVERSION, 'screen');
-//register_style('imstylefonts', IM_SITE_URL.'plugins/'.$thisfile
-//	.'/css/fonts/font-awesome/css/font-awesome.min.css', GSVERSION, 'screen');
-//queue_style('jqui', GSBACK);
-//queue_style('imstyle', GSBACK);
-//queue_style('imstylefonts', GSBACK);
-//queue_style('blueimp', GSBACK);
-
+if(!isset($_SESSION)){ session_start(); }
+//if(!isset($_SESSION['cat']) || is_null($_SESSION['cat'])) $_SESSION['cat'] = null;
 
 // Define constants
 include_once(__DIR__.'/_def.php');
-
 // Util
 include_once(IM_SOURCEPATH.'_Util.php');
 // Manager
@@ -42,11 +12,11 @@ include_once(IM_SOURCEPATH.'Manager.php');
 include_once(IM_SOURCEPATH.'ItemManager.php');
 
 /**
- * Core ItemManager's function, we use it to create an ItemManager instance
+ * ItemManager's core function, we use it to create a static ItemManager instance
  *
  * @param string $name
  *
- * @return Im\ItemManager instance
+ * @return Imanager\ItemManager instance
  */
 function imanager($name='')
 {
