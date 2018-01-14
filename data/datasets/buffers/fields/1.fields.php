2a596e9c099231b5f1bafe8209bad832a76974d3
Juri Ehret <juri.ehret@gmail.com>
2018-01-14 21:22:07 +0100

    data

--- data/datasets/buffers/fields/1.fields.php
+++ data/datasets/buffers/fields/1.fields.php
@@ -16,8 +16,31 @@
      'minimum' => 0,
      'maximum' => 0,
      'cssclass' => NULL,
-     'configs' => NULL,
+     'configs' => 
+    array (
+    ),
      'created' => 1515872202,
      'updated' => 1515872202,
+  )),
+  'password' => 
+  Imanager\Field::__set_state(array(
+     'categoryid' => 1,
+     'id' => 3,
+     'name' => 'password',
+     'label' => 'Enter your password',
+     'type' => 'password',
+     'position' => 3,
+     'default' => NULL,
+     'options' => 
+    array (
+    ),
+     'info' => NULL,
+     'required' => false,
+     'minimum' => 0,
+     'maximum' => 0,
+     'cssclass' => NULL,
+     'configs' => NULL,
+     'created' => 1515947553,
+     'updated' => 1515947553,
   )),
 ); ?>
