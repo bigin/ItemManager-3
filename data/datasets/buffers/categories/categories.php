6fb7aa13f3237d03f32800fe174cf8def7599f44
Juri Ehret <juri.ehret@gmail.com>
2018-01-22 15:19:13 +0100

    no message

--- data/datasets/buffers/categories/categories.php
+++ data/datasets/buffers/categories/categories.php
@@ -8,4 +8,13 @@
      'created' => 1516619824,
      'updated' => 1516619824,
   )),
+  2 => 
+  Imanager\Category::__set_state(array(
+     'id' => 2,
+     'position' => 2,
+     'name' => 'My Bla Category',
+     'slug' => 'my-bla-category',
+     'created' => 1516627628,
+     'updated' => 1516627628,
+  )),
 ); ?>
