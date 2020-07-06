29edf7161a116484206711d1ab7189d6aaede34f
Juri Ehret <juri.ehret@gmail.com>
2020-07-06 11:40:02 +0200

    new numeric Fields/Inputs drafted

--- imanager/lib/processors/InputInterface.php
+++ imanager/lib/processors/InputInterface.php
@@ -2,15 +2,15 @@
 
 interface InputInterface
 {
-	const        EMPTY_REQUIRED = -1;
+	const EMPTY_REQUIRED = -1;
 
-	const        ERR_MIN_LENGTH = -2;
+	const ERR_MIN_LENGTH = -2;
 
-	const        ERR_MAX_LENGTH = -3;
+	const ERR_MAX_LENGTH = -3;
 
-	const    WRONG_VALUE_FORMAT = -4;
+	const WRONG_VALUE_FORMAT = -4;
 
-	const     COMPARISON_FAILED = -5;
+	const COMPARISON_FAILED = -5;
 
 	const UNDEFINED_CATEGORY_ID = -6;
 
