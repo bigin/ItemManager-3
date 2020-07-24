ead177816bc57fd8888c15f45dd2ee4965d09834
Juri Ehret <juri.ehret@gmail.com>
2020-07-24 19:59:15 +0200

    no message

--- imanager/lib/processors/InputInterface.php
+++ imanager/lib/processors/InputInterface.php
@@ -2,21 +2,21 @@
 
 interface InputInterface
 {
-	const EMPTY_REQUIRED = -1;
+	const        EMPTY_REQUIRED = -1;
 
-	const ERR_MIN_LENGTH = -2;
+	const        ERR_MIN_LENGTH = -2;
 
-	const ERR_MAX_LENGTH = -3;
+	const        ERR_MAX_LENGTH = -3;
 
-	const WRONG_VALUE_FORMAT = -4;
+	const    WRONG_VALUE_FORMAT = -4;
 
-	const COMPARISON_FAILED = -5;
+	const     COMPARISON_FAILED = -5;
 
 	const UNDEFINED_CATEGORY_ID = -6;
 
 	public function __construct(Field $field);
 
-	public function prepareInput($value, $sanitize);
+	public function prepareInput($value, $sanitize = false);
 
 	public function prepareOutput();
 }
